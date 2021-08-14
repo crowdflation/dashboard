bf23462f8032999e0eae8bb7411a84cb49cb7c7a
Ignas <ignas@spottycash.net>
2021-08-14 16:45:29 +0100

    add extra parameters from client and deduplication

--- pages/api/walmart.ts
+++ pages/api/walmart.ts
@@ -21,6 +21,20 @@ function runMiddleware(req: any, res: any, fn: any) {
   })
 }
 
+function validateAndDenormalise(location:{latitude:number,longitude:number }) {
+  const dateTime = new Date();
+  //Round to hours so we can combine data
+  dateTime.setMinutes(0, 0, 0);
+
+  //Make location rought so that we can combine results
+  //TODO: use kilomoters for roughtness and check with Ekon team how rought it should be
+  const roughLocation = location?{latitude:parseFloat(location.latitude?.toString()).toFixed(3), longitude: parseFloat(location.longitude?.toString()).toFixed(3)}:null;
+  return function(item:any) {
+    return {...item, location: roughLocation, dateTime}
+  }
+}
+
+
 //TODO: Validation and assigning to a user
 import { connectToDatabase } from "../../lib/util/mongodb";
 export default async function handler(
@@ -29,7 +43,7 @@ export default async function handler(
 ) {
 
   // Run the middleware
-  await runMiddleware(req, res, cors)
+  await runMiddleware(req, res, cors);
 
   const {db} = await connectToDatabase();
   //TODO: put methods in constants
@@ -38,13 +52,26 @@ export default async function handler(
     //TODO: put shops in db or constants
       .collection("walmart")
       .find({})
+      .sort({dateTime:-1})
       .limit(10)
       .toArray();
     //TODO: put status code in constants
     return res.status(200).json(prices);
   } else if(req.method === 'POST') {
-    const prices = await db
-      .collection("walmart").insertMany(req.body);
+    const enriched = req.body.data.map(validateAndDenormalise(req.body.location));
+    //Add each item from the list
+    enriched.forEach(async function(item:any) {
+      const found = await db
+        .collection("walmart").findOne(item);
+      console.log('found', found);
+      if(!found) {
+        await db.collection("walmart").insertOne(item);
+      } else {
+        //If item is found just increment the counter
+        await db.collection("walmart").updateOne(item, {$inc: {count: 1}});
+      }
+    });
+
     return res.status(200).json({});
   }
   return res.status(404).json({message:'Invalid request type'});
