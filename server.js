7372c6e62f3efa7bd5915ab62e83e1047c1577a7
Ignas <ignas@spottycash.net>
2021-11-05 22:06:48 +0000

    Fix server not starting

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const next = require('next');
 const express = require('express');
 const sslRedirect = require('heroku-ssl-redirect').default; // to make it work with 'require' keyword.
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -20,6 +20,6 @@ app.prepare().then(() => {
   server.listen(port, err => {
     if (err) throw err;
 
-    console.log(`Server starts on ${PORT}.`);
+    console.log(`Server starts on ${port}.`);
   });
 });
