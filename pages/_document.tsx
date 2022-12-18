e52fec0cf596ef0482114ccbf2bb12166bd36a7d
Ignas Galvelis <igalvelis@gmail.com>
2022-12-18 17:27:07 +0000

    Fix google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,20 +6,25 @@ const USERSNAP_GLOBAL_API_KEY = '6eb9870f-70da-47c6-be24-13bae8f1b04d'
 export default class MyDocument extends Document {
     render() {
 
+        // @ts-ignore
         return (
             <Html lang="en">
                 <Head>
                     <script async src="https://www.googletagmanager.com/gtag/js?id=G-GHHYQ1HDTJ"></script>
-                    <script>
+                    <script dangerouslySetInnerHTML={{
+                        __html: `
                         window.dataLayer = window.dataLayer || [];
-                        function gtag(){
+                        function gtag() {
                             // @ts-ignore
-                            dataLayer.push(arguments)
+                            if(window) {
+                            // @ts-ignore
+                                window.dataLayer?.push(arguments);
+                            }
                         }
                         gtag('js', new Date());
 
-                        gtag('config', 'G-GHHYQ1HDTJ');
-                    </script>
+                        gtag('config', 'G-GHHYQ1HDTJ');`}}
+                    />
                 </Head>
                 <script
                     async
